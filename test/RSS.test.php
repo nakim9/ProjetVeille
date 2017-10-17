eeb99e1280d94c07c3014fc1d82ea0ea977fab2c
PESTRE Etienne <pestree@hostname.iut2.upmf-grenoble.fr>
2017-10-17 11:37:44 +0200

    classe & test

---
+++ test/RSS.test.php
@@ -0,0 +1,15 @@
+<?php
+      require_once('../model/RSS.class.php');
+
+      // Une instance de RSS
+      $rss = new RSS('http://www.lemonde.fr/m-actu/rss_full.xml');
+
+      // Charge le flux depuis le réseau
+      $rss->update();
+
+      // Affiche le titre
+      echo $rss->titre()."\n";
+
+      // Affiche les nouvelles
+      var_dump($rss->nouvelle());
+ ?>
